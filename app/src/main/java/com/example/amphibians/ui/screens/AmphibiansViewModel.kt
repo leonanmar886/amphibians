71973c509c818a3a49d5c65ce74ead610360c308
Leonan Marques <leonan.marques886@gmail.com>
2024-02-14 15:17:10 -0300

    home screen view model have been implemented

--- app/src/main/java/com/example/amphibians/ui/screens/AmphibiansViewModel.kt
+++ app/src/main/java/com/example/amphibians/ui/screens/AmphibiansViewModel.kt
@@ -1,7 +1,39 @@
 package com.example.amphibians.ui.screens
 
-sealed interface AmphibiansViewModel {
-  data class Success(val data: List<Amphibian>) : AmphibiansViewModel
-  data object Loading : AmphibiansViewModel
-  data object Error : AmphibiansViewModel
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import kotlinx.coroutines.launch
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import com.example.amphibians.data.AmphibiansRepository
+import com.example.amphibians.model.Amphibian
+
+sealed interface AmphibiansUiState {
+  data class Success(val data: List<Amphibian>) : AmphibiansUiState
+  data object Loading : AmphibiansUiState
+  data object Error : AmphibiansUiState
+}
+
+class AmphibiansViewModel(private val repository: AmphibiansRepository): ViewModel() {
+  var amphibiansUiState: AmphibiansUiState by mutableStateOf(AmphibiansUiState.Loading)
+    private set
+  
+  init {
+    getAmphibians()
+  }
+  
+  fun getAmphibians() {
+    viewModelScope.launch {
+      amphibiansUiState = try {
+        AmphibiansUiState.Success(repository.getAmphibians())
+      } catch (e: Exception) {
+        AmphibiansUiState.Error
+      }
+    }
+  }
 }
